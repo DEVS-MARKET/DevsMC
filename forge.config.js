71539de408b429719bce2fc9708982b57c06040c
Krzysztof Haller <contact@khaller.com>
2024-08-23 20:14:15 +0200

    Add a x64 MacOS building

--- forge.config.js
+++ forge.config.js
@@ -43,7 +43,7 @@ module.exports = {
       name: '@electron-forge/maker-dmg',
       config: {
         icon: __dirname + '/icons/icon.icns',
-        name: 'DevsMC',
+        name: 'DevsMC - ' + process.arch,
       },
     },
     {
